24e84ae3c75c5326466ac86d356cf48260648424
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-04-23 15:38:39 -0700

    update

--- random_note.py
+++ random_note.py
@@ -4,4 +4,4 @@ def randomNote():
     # Implement this
     note_name = notes[random.randint(0,len(notes))]
     rhythm = str(2 ** (random.randint(1,4)))
-    return None # what should we return?
+    return note_name + rhythm
