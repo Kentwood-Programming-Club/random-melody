76041c80cae49712f718fd21e79474e794f8e0bd
Nathaniel D'Rozario <nsdrozario@gmail.com>
2021-04-24 00:00:45 -0700

    remove print

--- generator.py
+++ generator.py
@@ -3,5 +3,4 @@ from music21 import *
 note_list = []
 for i in range(10):
     note_list.append(randomNote())
-    converter.parse("tinynotation: 4/4 " + " ".join(note_list)).show()
-    print(note_list)
+converter.parse("tinynotation: 4/4 " + " ".join(note_list)).show()
